29a6a885c4fdda33d132c4496ab7750deacce602
Radosław Wierzgała <rad.wierzgala@gmail.com>
2023-12-30 23:06:46 +0100

    seems to work

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from person import Person
 all_persons = []
 radek = Person(mail="radocha.w@gmail.com", name="Radek", day=30, month=12)
 all_persons.append(radek)
-jacek = Person(mail="rad.wierzgala@gmail.com", name="Jacek", day=30, month=12)
+jacek = Person(mail="rad.wierzgala@gmail.com", name="Jacek", day=31, month=12)
 all_persons.append(jacek)
 
 if __name__ == "__main__":
@@ -15,8 +15,7 @@ if __name__ == "__main__":
         for person in all_persons:
             if person.day == today.day and person.month == today.month:
                 person.send_mail()
-                
-                
-        time.sleep(3)
-        # time.sleep(60*60*24)
+                               
+        # time.sleep(3000)
+        time.sleep(60*60*24)
         
--- person.py
+++ person.py
@@ -1,4 +1,16 @@
-class Person:
+import smtplib
+from email.mime.text import MIMEText
+
+mail_content = """
+        Hello,
+        Today is Your birthsday
+    """
+subject = "test title"
+key = "dbjztgprpebwissj" 
+my_mail = "rad.wierzgala@gmail.com"
+# text = f"Subjetct: {subject} \n\n {mail_content}"
+    
+class Person:   
     def __init__(self, name, mail, day, month):
         self.name = name
         self.mail = mail
@@ -6,11 +18,17 @@ class Person:
         self.month = month
 
     
-    def send_mail():
-        print("Mail sended")
+    def send_mail(self):
+        msg = MIMEText(mail_content)
+        msg['Subject'] = subject
+        msg['From'] = my_mail
+        msg['To'] = self.mail
+    
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(my_mail, key)
+        server.sendmail(my_mail, self.mail, msg.as_string())
+        
+        print(f"Mail sended to {self.name}")
+        
 
-    def mail_content():
-        return """
-        Hello,
-        Today is Your birthsday
-    """
